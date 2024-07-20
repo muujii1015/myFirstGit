fc283997e775abefb81d6090bfc240188b1dd939
muujii1015 <munkhjargal.lkhagvasuren@griffithuni.edu.au>
2024-07-20 15:07:15 +1000

    Resolved merge conflict

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+console.log("I caused a merge conflict");
+function addNumbers(a, b) {
+    return a + b;
+}
+const sum = addNumbers(5, 10);
+console.log("Sum of 5 + 10 is:", sum);

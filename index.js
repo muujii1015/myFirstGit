55a025256bf5c57eb994d4a46cd4e85d6fc9d0fa
muujii1015 <munkhjargal.lkhagvasuren@griffithuni.edu.au>
2024-07-20 15:01:28 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+console.log("Hello World");
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+const sum = addNumbers(5, 10);
+console.log("Sum of 5 + 10 is:", sum);
